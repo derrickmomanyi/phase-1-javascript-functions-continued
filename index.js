8365a0d3b3136481a9284b30601dca11ae7fa8ad
Derrick Momanyi <derrick.momanyi@student.moringaschool.com>
2022-11-22 19:36:18 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}   
+console.log(saturdayFun());
+
+
+function mondayWork(activity= "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+function wrapAdjective(){
+    const innerFunction = function (name = "special"){
+        return `You are *${name}*!`;
+        
+
+    };
+      return innerFunction; 
+      }
+
+ console.log(wrapAdjective())     
+
